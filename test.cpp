a1d10e0486f9a4ba80e5006227f9bfa46f3a7dbe
Max Kusatz <max@trialserver.de>
2023-03-06 14:27:20 +0100

    added cpp test

--- test.cpp
+++ test.cpp
@@ -5,8 +5,8 @@
 
 int main() {
     // Load the graph from a file
-    auto reader = NetworKit::METISGraphReader;
-    NetworKit::Graph G = reader.read("../instances/105.txt");
+    auto reader = NetworKit::METISGraphReader();
+    NetworKit::Graph G = reader.read("../instances/104.txt");
 
     // Calculate the betweenness centrality
     NetworKit::Betweenness centrality(G);
